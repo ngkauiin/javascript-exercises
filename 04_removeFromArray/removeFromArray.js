440fa7c32a781997bcd745747c6f1c1e992d8677
kuaiin.v.ng@gmail.com <ngkauiin@gmail.com>
2025-04-29 16:55:16 +1000

    Use filter to remove items
    toSpliced() method can't only remove one item in the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function(array, removeItem) {
-  return array.toSpliced(array.indexOf(removeItem),1);
+const removeFromArray = function(array, ...removeItem) {
+  let i = 0;
+  while (i<removeItem.length) {
+    array = array.filter(matchItem);
+    i++;
+  }
+  return array;
+
+  function matchItem(item) {
+    return item !== removeItem[i];
+  }
 };
 
 // Do not edit below this line
