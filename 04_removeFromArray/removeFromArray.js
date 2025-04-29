f25c90ba381b48cbc1a80610efa0bda844fd910b
kuaiin.v.ng@gmail.com <ngkauiin@gmail.com>
2025-04-29 16:18:37 +1000

    find the index of remove-item and remove from the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removeItem) {
+  return array.toSpliced(array.indexOf(removeItem),1);
 };
 
 // Do not edit below this line
