8c2310e42da621908dffe8af34576dad1452ca3b
kuaiin.v.ng@gmail.com <ngkauiin@gmail.com>
2025-04-29 20:05:53 +1000

    pass first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+  let sum = 0;
+  for (i=numOne; i<=numTwo;i++) {
+    sum +=i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
