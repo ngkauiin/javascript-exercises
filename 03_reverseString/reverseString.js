1148943c52d52a3ca59bc22968670322071a031c
kuaiin.v.ng@gmail.com <ngkauiin@gmail.com>
2025-04-29 16:01:25 +1000

    reverse string using for loop and concat() method; solution convert string to array then reverse() then array to string using join()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let newString = "";
+  for (i=0; i<string.length; i++) {
+    newString = newString.concat(string.at(-1+i*-1));
+  }
+  return newString;
 };
 
 // Do not edit below this line
