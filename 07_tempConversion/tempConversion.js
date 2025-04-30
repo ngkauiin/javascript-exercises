68f63fa0f04b24bc12eb10bdd674071034d5ce40
kuaiin.v.ng@gmail.com <ngkauiin@gmail.com>
2025-04-30 11:59:31 +1000

    create a function to round number to 1 decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -4,6 +4,10 @@ const convertToCelsius = function() {
 const convertToFahrenheit = function() {
 };
 
+function roundToOneDecimalPlace(num) {
+  return Math.round(num*10)/10;
+}
+
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
