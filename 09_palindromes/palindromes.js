5f38e6bdfa0316f8044b8aa13eadbee680a9f4a7
kuaiin.v.ng@gmail.com <ngkauiin@gmail.com>
2025-05-06 15:21:59 +1000

    use filter, map and every method to complete the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const array = string.split('');
+  const filteredArray = array
+                          .filter((char) => char.match(/[a-z0-9]/i))
+                          .map((char) => char.toLowerCase());
+  const reverseArray = filteredArray.slice().reverse();
+  return filteredArray.every((letter, index) => letter === reverseArray[index]);
 };
 
 // Do not edit below this line
