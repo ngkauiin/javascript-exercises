8ca80e14f4f5a33800007840415e9ebe8173d1ba
kuaiin.v.ng@gmail.com <ngkauiin@gmail.com>
2025-05-08 13:49:16 +1000

    return 'OOPS' if number is negative

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,5 @@
 const fibonacci = function(number) {
-  // create an object {0:0 ,1:1}
-  // Do the given number (n) - 2 times (use for loop) more calculation to store as n: object[n-1]+object[n-2]
-  // return the last element
+  if (number < 0) return 'OOPS'
 
   let fibonacciNumber = {
     0:0,
@@ -11,10 +9,7 @@ const fibonacci = function(number) {
   for (let n = 2; n <= number; n++) {
     fibonacciNumber[n] = fibonacciNumber[n-1]+fibonacciNumber[n-2];
   }
-  
   return fibonacciNumber[number];
-
-
 };
 
 // Do not edit below this line
