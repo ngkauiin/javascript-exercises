4e7b5f6dee4ac70d306a5c14d5572d7558ff2973
kuaiin.v.ng@gmail.com <ngkauiin@gmail.com>
2025-05-08 13:45:32 +1000

    create object, and use for loop to calculate each fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  // create an object {0:0 ,1:1}
+  // Do the given number (n) - 2 times (use for loop) more calculation to store as n: object[n-1]+object[n-2]
+  // return the last element
+
+  let fibonacciNumber = {
+    0:0,
+    1:1,
+  }
+
+  for (let n = 2; n <= number; n++) {
+    fibonacciNumber[n] = fibonacciNumber[n-1]+fibonacciNumber[n-2];
+  }
+  
+  return fibonacciNumber[number];
+
 
 };
 
