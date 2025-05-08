b8383a08c315858f1e0ef42eb302a88780171966
kuaiin.v.ng@gmail.com <ngkauiin@gmail.com>
2025-05-08 14:07:46 +1000

    calculate each person age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const oldestPerson = people.sort((personA, personB) => {
+    calculatePersonAge(personA);
+    calculatePersonAge(personB);
 
+  })
 };
+
+function calculatePersonAge(person) {
+  if ('yearOfDeath' in person) {
+    person['age'] = person.yearOfDeath - person.yearOfBirth;
+  } else {
+    person['age'] = Date.getFullYear() - person.yearOfBirth;
+  }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
